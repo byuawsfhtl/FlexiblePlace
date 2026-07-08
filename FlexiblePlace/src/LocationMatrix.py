3c4758cd08884f4150c0b3e6b96c415919e09db5
ConnerH1123 <hconner375@gmail.com>
2026-07-08 16:56:19 -0600

    psuedocode created for the link function

--- FlexiblePlace/src/LocationMatrix.py
+++ FlexiblePlace/src/LocationMatrix.py
@@ -41,22 +41,61 @@ class LocationMatrix:
             while len(row) < new_size:
                 row.append(LocationComponent((i,len(row))))
 
-    # def _link(location_matrix: LocationMatrix, row_a: int, column_a: int, component_b: tuple[int,int]):
-        # component_a: tuple[int, int] = (row_a, column_b)
-        # 
+    # def _link(self, component_a: LocationComponent, component_b: LocationComponent):
+        # if _illegal_move(component_a, component_b): # Not finished
+            # return
+        # closest_component: LocationComponent = _get_closest(component_a, component_b)
+        # distance: int = abs(component_a.column - component_b.column)
+        # move(closest_component, distance)
+        # component_a.link(component_b)
+    
+    # def _illegal_move(self, component_a: LocationComponent, component_b: LocationComponent) -> bool:
+        # if not component_a or not component_b:
+            # return True
+        ## if there is a link in the way return True else return False
+
+    # def _get_closest(self, component_a: LocationComponent, component_b: LocationComponent):
+        # if component_a.column < component_b.column:
+            # return component_a
+        # else:
+            # return component_b
+        
+    # def _move(self, component: LocationComponent, distance: int):
+        # for i in range(distance):
+            # shift_right(component)
+    
+    # def _shift_right(self, component: LocationComponent)
+        # current_column: int = component.column
+        # if current_column == (self.column_count - 1):
+            # self._resize(column_count + 1)
+            # self._shift_right(component)
+        # elif not component:
+            # return
+        # else:
+            # row: int = component.row
+            # column: int = component.column
+            # self.matrix[row][column] = LocationComponent(([row][column]))
+            # component.column += 1
+            # column += 1
+            # _shift_right(self.matrix[row][column])
+            # self.matrix[row][column] = component
+            # for linked_component in components.links:
+                # distance: int = abs(component.column - linked_component.column)
+                # self._move(linked_component, distance)
 
 def align(location_matrix: LocationMatrix):
     for row in range(len(location_matrix.matrix)):
         while (True):
-            match: tuple[int, int] = _find_best_match(location_matrix, row)
+            match: tuple[tuple[int, int], tuple[int, int]] = _find_best_match(location_matrix, row)
             if not match:
                 break
-            matched_component: LocationComponent = location_matrix.matrix[match[0]][match[1]]
+            component_a: LocationComponent = location_matrix.matrix[match[0][0]][match[0][1]]
+            component_b: LocationComponent = location_matrix.matrix[match[1][0]][match[1][1]]
             # _link(location_matrix, row, column, matched_component)
 
-def _find_best_match(location_matrix: LocationMatrix, row: int) -> tuple[int, int]:
-    best_score: float = 80.0 # This (80) is the threshold for a match
-    best_match: tuple[int, int] | tuple = ()
+def _find_best_match(location_matrix: LocationMatrix, row: int) -> tuple[tuple[int, int], tuple[int, int]]:
+    best_score: float = 80.0    # This (80) is the threshold for a match
+    best_match: tuple[tuple[int, int], tuple[int, int]] | tuple = ()
     for column in range(len(location_matrix.matrix[row])):
         for i in range(row):
             for j in range(location_matrix.column_count):
@@ -65,7 +104,7 @@ def _find_best_match(location_matrix: LocationMatrix, row: int) -> tuple[int, in
                 score: float = CompareLocations.basic_comparison_algorithm(component_a, component_b)
                 if score > best_score:
                     best_score = score
-                    best_match = (i, j)
+                    best_match = ((row,column), (i, j))
     return best_match
 
     
