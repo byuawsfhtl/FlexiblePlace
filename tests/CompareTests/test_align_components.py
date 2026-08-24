2afb2481fbaa83e485038a9258d1deec072b3275
ConnerH1123 <hconner375@gmail.com>
2026-08-24 16:48:14 -0600

    align_components test changed

--- tests/CompareTests/test_align_components.py
+++ tests/CompareTests/test_align_components.py
@@ -1,12 +1,14 @@
 from FlexiblePlace.src.Compare import Compare
+from unittest.mock import patch
 
 class TestAlignComponents:
-    def test_align_locations(self) -> None:
-        """Tests that the align_locations method works"""
-        place_a = ["washington", "united states"]
-        place_b = ["walla walla", "washingon"]
-        expected = [
-            ["", "washington", "united states"],
-            ["walla walla", "washingon", ""]
-        ]
-        assert Compare.align_components(place_a, place_b) == expected
+    def test_creates_LocationMatrix_object(self) -> None:
+        """Tests that the align_locations uses the LocationMatrix to align locations"""
+        with patch("FlexiblePlace.src.Compare.LocationMatrix") as mock_location_matrix:
+            place_a = ["a", "b"]
+            place_b = ["b", "c"]
+            expected = [["a", "b", ""], ["", "b", "c"]]
+            mock_location_matrix.return_value.get_locations.return_value = expected
+            actual = Compare.align_components(place_a, place_b)
+            mock_location_matrix.assert_called_once_with([place_a, place_b])
+            assert expected == actual
