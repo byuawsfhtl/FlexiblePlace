402c4a81c3ae0b9473a64595363b2533a133d54b
ConnerH1123 <hconner375@gmail.com>
2026-08-31 13:56:46 -0600

    tests added

--- tests/CompareTests/CompareTests.py
+++ tests/CompareTests/CompareTests.py
@@ -1,5 +1,41 @@
 from FlexiblePlace.src.Compare import Compare
+from unittest.mock import patch
 import pytest
+
+class TestAlignComponents:
+    def test_creates_LocationMatrix_object(self) -> None:
+        """Tests that the align_locations uses the LocationMatrix to align locations.
+        
+        The align functionality is already rigorously tested in the LocationMatrix tests, so this test
+        only checks that the align_components method utilizes the LocationMatrix to align components"""
+        with patch("FlexiblePlace.src.Compare.LocationMatrix") as mock_location_matrix:
+            place_a = ["a", "b"]
+            place_b = ["b", "c"]
+            expected = [["a", "b", ""], ["", "b", "c"]]
+            mock_location_matrix.return_value.get_locations.return_value = expected
+            actual = Compare.align_components(place_a, place_b)
+            mock_location_matrix.assert_called_once_with([place_a, place_b])
+            assert expected == actual
+
+class TestCompareEachComponent:
+    @staticmethod
+    def mock_ratio(place_a, place_b) -> float:
+        """A mock of the fuzz.ratio method that hard codes outputs for specific inputs."""
+        if place_a == "walla walla" and place_b == "walla walla":
+            return 100.0
+        elif place_a == "united states" and place_b == "usa":
+            return 30.0
+        else:
+            return 0
+        
+    def test_compare_each_location(self) -> None:
+        """Tests that the method correctly pairs up and compares the components"""
+        with patch("FlexiblePlace.src.Compare.fuzz") as mock_fuzz:
+            mock_fuzz.ratio = TestCompareEachComponent.mock_ratio
+            place_a = ["walla walla", "washington", "united states"]
+            place_b = ["walla walla", "wrongstate", "usa"]
+            expected = [100.0, 0.0, 30.0]
+            assert Compare.compare_each_component([place_a, place_b]) == expected
 
 class TestAdjustScores:
     """Tests that the scores are adjusted correctly, assuming that first score represents the most specific component (e.g. city)"""
