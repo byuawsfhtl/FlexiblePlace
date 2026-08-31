09b4791d1b03922784121182bdb8513eeb6c8685
ConnerH1123 <hconner375@gmail.com>
2026-08-31 14:37:51 -0600

    Combine class added

---
+++ FlexiblePlace/src/Combine.py
@@ -0,0 +1,19 @@
+from FlexiblePlace.src.LocationMatrix import LocationMatrix
+
+class Combine:
+    @staticmethod
+    def align_components(location_a: list[str], location_b: list[str]) -> list[list[str]]:
+        """Aligns two lists of strings representing locations.
+
+        Relies heavily on the built-in component alignment functionality of the LocationMatrix class.
+
+        Args:
+            location_a (list[str]): First list of location component strings.
+            location_b (list[str]): Second list of location component strings.
+
+        Returns:
+            list[list[str]]: A list containing the two aligned location component lists."""
+        location_matrix = LocationMatrix([location_a, location_b])
+        return location_matrix.get_locations()
+
+    
