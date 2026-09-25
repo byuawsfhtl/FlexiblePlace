5dfa3de4d15771680faffe14f4b1c8cf860add2b
ConnerH1123 <hconner375@gmail.com>
2026-09-25 16:00:57 -0600

    readability - updated docstring

--- tests/compare_placesTests/test_compare_places_not_a_match.py
+++ tests/compare_placesTests/test_compare_places_not_a_match.py
@@ -38,7 +38,7 @@ class TestCompareTwoPlacesNotAMatch:
         assert result < 70
 
     def test_different_specificity(self) -> None:
-        """Different locations of different specificity levels should return a score lower than 75"""
+        """Different locations of different specificity levels should return a score lower than 10"""
         place1 = FlexiblePlace("Walla Walla, Washington, United States")
         place2 = FlexiblePlace("Belgium")
         result = compare_places(place1, place2)
